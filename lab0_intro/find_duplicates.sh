a10c09bba3f3855d5fc7a16365686ff4680de51a
dagomezb <dgomez921@gmail.com>
2015-02-03 08:06:25 -0500

    Create find_duplicates.sh

---
+++ lab0_intro/find_duplicates.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+clear
+
+echo “Script Duplicados“
+
+find -not -empty -type f -printf "%s\n" | sort -rn | uniq -d | xargs -I{} -n1 find -type f -size {}c -print0 | xargs -0 md5sum | sort | uniq -w32 --all-repeated=separate
