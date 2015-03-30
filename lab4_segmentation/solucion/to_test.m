352e25e1d8a94f7498219bdec6bd4b9cdfe01680
dagomezb <dgomez921@gmail.com>
2015-03-30 10:45:04 -0500

    Update Solucion

--- lab4_segmentation/solucion/to_test.m
+++ lab4_segmentation/solucion/to_test.m
@@ -5,7 +5,9 @@ clc;
 %leer imagen
 imgoriginal = imread('8068.jpg');
 imshow(imgoriginal)
+tic
 segmented = segment_by_clustering(imgoriginal,'rgb','watershed',3);
+toc
 figure;
 image(segmented);
 colormap colorcube;
